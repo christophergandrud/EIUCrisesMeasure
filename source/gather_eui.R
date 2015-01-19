cec879a47309cc221663af9c714f53adb5f29dc7
christophergandrud <christopher.gandrud@gmail.com>
2015-01-19 16:36:41 +0100

    added gather file

---
+++ source/gather_eui.R
@@ -0,0 +1,30 @@
+library(RCurl)
+library(dplyr)
+
+setwd('~/Desktop/eui_raw/')
+
+base_url <- 'http://nationallizenzen.zbw.eu/handle/10836/'
+
+for (i in 1:22999){
+    Sys.sleep(1)
+    # Download page to find country-time link
+    temp <- paste0(base_url, i)  %>% getURL() %>% as.data.frame()
+
+    # Extract country-time link
+    if (grepl('.*?zbwhtml(.*?)html.*', temp[1, 1])){
+        temp_sub <- sub('.*?zbwhtml(.*?)html.*', "\\1", temp[1, 1])
+
+        # Create download link and extract county-month-year file name
+        new_url <- paste0('http://nationallizenzen.zbw.eu/zbwhtml', temp_sub,
+                        'html?page=full')
+
+        temp_file_name <- temp_sub %>%
+            gsub(paste0('/10836/', i, '/Country%20Report%20'), '', .) %>%
+            gsub('%20', '_', .) %>%
+            paste0(., 'html')
+
+        download.file(new_url, temp_file_name)
+    }
+}
+
+2399
