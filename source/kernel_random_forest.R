625cfc8b70609a3fdd321ebdf0ec55a11e1f3993
Christopher Gandrud <christopher.gandrud@gmail.com>
2015-05-21 15:42:27 +0200

    update random forest

---
+++ source/kernel_random_forest.R
@@ -0,0 +1,110 @@
+# ---------------------------------------------------------------------------- #
+# Compare Kernal PCA to Stem Frequency
+# Christopher Gandrud
+# 21 May 2015
+# MIT License
+# ---------------------------------------------------------------------------- #
+
+# Set working directory of parsed texts. Change as needed.
+setwd('~/Desktop/eiu/eiu_extracted/')
+
+# Load packages
+library(tm)
+library(SnowballC)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(ggplot2)
+library(gridExtra)
+library(tidyr)
+library(rio)
+
+# Function to count the number of words in a string
+wordcount <- function(x) sapply(gregexpr("\\W+", x), length) + 1
+
+# Load corpus
+clean_corpus_full <- Corpus(DirSource()) %>%
+    tm_map(removeWords, c(stopwords('english'), 'the'), mc.cores = 1) %>%
+    tm_map(stemDocument, mc.cores = 1) %>%
+    tm_map(stripWhitespace) %>%
+    # Results correspond to priors much more closely when case is retained
+    # tm_map(content_transformer(tolower), mc.cores = 1) %>%
+    tm_map(removePunctuation, mc.cores = 1) %>%
+    tm_map(removeNumbers, mc.cores = 1)
+
+# Kernal length
+length_spec = 5
+
+clean_corpus_full <- clean_corpus_full %>% as.list
+
+# Keep texts that have more words than the kernal length
+keep_vec <- vector()
+for (i in 1:length(clean_corpus_full)) {
+    temp <- clean_corpus_full[[i]]$content
+    more_length <- wordcount(temp) > length_spec
+    if (isTRUE(more_length)) keep_vec <- c(keep_vec, i)
+}
+
+clean_corpus <- clean_corpus_full[keep_vec] %>% as.VCorpus %>%
+                    DocumentTermMatrix
+term_freq <- inspect(removeSparseTerms(clean_corpus, 0.9)) %>% as.data.frame
+
+## Drop countries with fewer than 5 observations
+# Create date-country labels
+date_country <- row.names(term_freq) %>% gsub('\\.txt', '', .) %>%
+    str_split_fixed('_', n = 2) %>%
+    as.data.frame(stringsAsFactors = F)
+date_country[, 2] <- gsub('-', ' ', date_country[, 2])
+names(date_country) <- c('date_date', 'country_country')
+
+term_freq <- cbind(date_country, term_freq)
+
+date_country$fake_fake <- 1
+date_country <- date_country %>% group_by(country_country) %>%
+    mutate(obs_sum = sum(fake_fake)) %>%
+    filter(obs_sum > 5) %>% select(-fake_fake, -obs_sum)
+
+term_freq <- merge(date_country, term_freq, by = c('country_country', 'date_date'))
+term_freq <- term_freq %>% select(-country_country, -date_date)
+
+#### Download KPCA results ####
+kpca <- import('~/git_repositories/EIUCrisesMeasure/data/results_kpca_rescaled.csv')
+
+#### Combine ####
+cor_pca <- function(var) {
+    temp_function <- function(x) cor(kpca[, var], x)
+    corrs <- apply(term_freq, 2, temp_function)
+    corrs <- data.frame(terms = names(term_freq), correlations = corrs)
+    corrs_sorted <- corrs %>% arrange(desc(correlations))
+    return(corrs_sorted)
+}
+
+c1_cor <- cor_pca('C1')
+c2_cor <- cor_pca('C2')
+c3_cor <- cor_pca('C3')
+
+
+
+#### Random forest test ####
+library(randomForest)
+
+comb <- cbind(kpca$C1, term_freq)
+comb <- dplyr::rename(comb, C1 = `kpca$C1`)
+
+addq <- function(x) paste0("`", x, "`")
+
+form <- paste('C1 ~', paste(addq(names(term_freq)), collapse = ' + ')) %>%
+          as.formula
+#[sample(1:ncol(term_freq), 350)]
+rf1 <- randomForest(form, data = comb, importance = T, do.trace = 100)
+
+term_names <- rownames(importance(rf1))
+term_importance <- cbind.data.frame(term_names, importance(rf1))
+term_importance <- term_importance %>% arrange(desc(`%IncMSE`))
+
+
+#### Try bigrf ####
+library(randomForestSRC)
+
+rfsrc_test <- rfsrc(form, data = comb)
+plot(rfsrc_test)
