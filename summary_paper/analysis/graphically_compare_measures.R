e69f44b5274bda6e3307350aed06d66ba418aa8c
christophergandrud <christopher.gandrud@gmail.com>
2015-05-26 09:40:43 +0200

    rearrange source files

--- summary_paper/analysis/graphically_compare_measures.R
+++ summary_paper/analysis/graphically_compare_measures.R
@@ -27,8 +27,10 @@ perceptions$iso2c <- countrycode(perceptions$country, origin = 'country.name',
 perceptions <- perceptions %>% dplyr::select(iso2c, date, C1, C2, C3, C1_ma)
 perceptions$date <- ymd(perceptions$date)
 
-perceptions$country <- countrycode(perceptions$iso2c, origin = 'iso2c', 
+perceptions$country <- countrycode(perceptions$iso2c, origin = 'iso2c',
                                    destination = 'country.name')
+
+perceptions$
 
 ## Import Romer and Romer (2015)
 romer_romer <- import('data/alternative_measures/cleaned/rommer_romer.csv')
@@ -37,10 +39,10 @@ romer_romer <- romer_romer %>% select(-country)
 
 romer_romer$rr_rescale <- range01(romer_romer$rr_distress)
 
-romer_romer$country <- countrycode(romer_romer$iso2c, origin = 'iso2c', 
+romer_romer$country <- countrycode(romer_romer$iso2c, origin = 'iso2c',
                                     destination = 'country.name')
 
-
+romer_romer$Source <- 'Romer/Romer'
 
 ## Load Reinhart and Rogoff
 source('data/alternative_measures/reinhart_rogoff.R')
@@ -75,22 +77,22 @@ lv_se <- cbind(lv_se_start, lv_se_end)
 lv_se$Source <- 'Laeven/Valencia'
 
 comb_se <- rbind(rr_bc, lv_se)
-    
-comb_se$country <- countrycode(comb_se$iso2c, origin = 'iso2c', 
+
+comb_se$country <- countrycode(comb_se$iso2c, origin = 'iso2c',
                                  destination = 'country.name')
 
 #### Compare to LV ####
 compare_to_dummy <- function(data_cont, data_dummy, id) {
     temp_cont <- subset(data_cont, country == id)
     temp_dummy <- subset(data_dummy, country == id)
-    
+
     if (nrow(temp_dummy) == 0) {
         ggplot(data = temp_cont, aes(date, C1_ma)) +
             geom_line() +
             stat_smooth(se = F, colour = 'black') +
             scale_y_continuous(limits = c(0, 1),
                                breaks = c(0, 0.25, 0.5, 0.75, 1)) +
-            ggtitle(id) + xlab('') + 
+            ggtitle(id) + xlab('') +
             ylab('Perceptions of \n Financial Market Conditions\n') +
             theme_bw()
     } else if (nrow(temp_dummy)) {
@@ -98,19 +100,19 @@ compare_to_dummy <- function(data_cont, data_dummy, id) {
             geom_line(data = temp_cont, aes(date, C1_ma)) +
             stat_smooth(data = temp_cont, aes(date, C1_ma),
                         se = F, colour = 'black') +
-            geom_rect(data = temp_dummy, aes(xmin = start, xmax = end, 
+            geom_rect(data = temp_dummy, aes(xmin = start, xmax = end,
                                                 ymin = -Inf, ymax = Inf,
-                                                fill = Source), 
+                                                fill = Source),
                       alpha = 0.4) +
             scale_fill_manual(values = c("#D8B70A", "#972D15")) +
             scale_y_continuous(limits = c(0, 1),
                                breaks = c(0, 0.25, 0.5, 0.75, 1)) +
-            ggtitle(id) + xlab('') + 
+            ggtitle(id) + xlab('') +
             ylab('Perceptions of \n Financial Market Conditions\n') +
             theme_bw() +
-            theme(legend.position = "none") 
+            theme(legend.position = "none")
     }
-} 
+}
 
 
 country_vector <- unique(perceptions$country)
@@ -118,28 +120,27 @@ kpca_list <- list()
 for (i in country_vector) {
     message(i)
     kpca_list[[i]] <- suppressMessages(
-            compare_to_dummy(data_cont = perceptions, 
+            compare_to_dummy(data_cont = perceptions,
                              data_dummy = comb_se,
                              id = i))
 }
 
-# Plot selection
-select_countries_1 <- c('Argentina', 'Australia', 'Austria', 'Belgium', 
-                      'Brazil', 'Canada', 'China', 'Denmark', 
-                      'France', 'Germany', 'Greece', 'Iceland', 
-                      'India', 'Ireland', 'Italy', 'Japan', 
+# Plot selection (1)
+select_countries_1 <- c('Argentina', 'Australia', 'Austria', 'Belgium',
+                      'Brazil', 'Canada', 'China', 'Denmark',
+                      'France', 'Germany', 'Greece', 'Iceland',
+                      'India', 'Ireland', 'Italy', 'Japan',
                       'Singapore', 'Spain',  'United Kingdom', 'United States'
                       )
-pdf(file = 'summary_paper/analysis/figures/compare_to_lv_rr.pdf', width = 15, 
+pdf(file = 'summary_paper/analysis/figures/compare_to_lv_rr.pdf', width = 15,
     height = 15)
     do.call(grid.arrange, kpca_list[select_countries_1])
 dev.off()
 
-
-# Plot selection
-select_countries_2 <- c('Bulgaria', 'Czech Republic', 'Estonia', 'Hungary', 'Latvia', 'Lithuania', 
+# Plot selection (2)
+select_countries_2 <- c('Bulgaria', 'Czech Republic', 'Estonia', 'Hungary', 'Latvia', 'Lithuania',
                         'Russian Federation', 'Slovenia', 'Ukraine')
-pdf(file = 'summary_paper/analysis/figures/compare_to_lv_rr_2.pdf', width = 15, 
+pdf(file = 'summary_paper/analysis/figures/compare_to_lv_rr_2.pdf', width = 15,
     height = 15)
     do.call(grid.arrange, kpca_list[select_countries_2])
 dev.off()
