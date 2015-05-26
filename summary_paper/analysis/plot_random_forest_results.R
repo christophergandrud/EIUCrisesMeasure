58b00b194a86f3087df19ac6f2e11f8d61b48830
christophergandrud <christopher.gandrud@gmail.com>
2015-05-26 13:11:27 +0200

    minor

--- summary_paper/analysis/plot_random_forest_results.R
+++ summary_paper/analysis/plot_random_forest_results.R
@@ -10,7 +10,21 @@ setwd('/git_repositories/EIUCrisesMeasure/')
 
 # Load packages
 library(rio)
+library(dplyr)
 library(ggplot2)
 
 # Load results
-imp <- import('data/')
+imp <- import('data/random_forest_var_imp_C1.csv')
+
+# Convert to percentage
+imp$variable_importance <- imp$variable_importance * 100
+
+# Find 30 top
+imp <- imp %>% arrange(desc(variable_importance))
+imp <- imp[1:30, ]
+
+
+# Plot
+ggplot(imp, aes(variable_importance, word_stem)) +
+    geom_point() +
+    theme_bw()
