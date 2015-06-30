819be860562c9662235d809ac0ef5f26c1aa10bf
christophergandrud <christopher.gandrud@gmail.com>
2015-06-30 13:59:44 +0200

    residuals table added

---
+++ summary_paper/analysis/application_regression.R
@@ -0,0 +1,32 @@
+# ---------------------------------------------------------------------------- #
+# Application Tables
+# Christopher Gandrud
+# MIT LICENSE
+# ---------------------------------------------------------------------------- #
+
+# Load required packages 
+library(devtools)
+library(dplyr)
+library(stargazer)
+
+# Run regressions
+source_url('https://raw.githubusercontent.com/christophergandrud/financial_crisis_fiscal_policy/master/analysis_data/spending_regress_tables_v2.R')
+
+# Reset working directory
+setwd('/git_repositories/EIUCrisesMeasure/summary_paper/')
+
+# Create residuals table
+stargazer(m_r1, m_r2,
+          dep.var.labels = c('Debt', 
+                             'Debt Residuals'),
+          covariate.labels = c('Debt$_{t-1}$', 
+                               'Output Gap',
+                               'Debt Resid.$_{t-1}$',
+                               'EPFMS'),
+          omit = 'iso2c', omit.labels = 'country fixed effects',
+          float = F,
+          omit.stat = c('f', 'ser'),
+          font.size = 'tiny',
+          out = 'tables/debt_residual_regress.tex'
+)
+
