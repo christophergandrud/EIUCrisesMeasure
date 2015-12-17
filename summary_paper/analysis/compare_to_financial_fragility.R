5c95dfc452393108232126be63465b6daf4507a4
christophergandrud <christopher.gandrud@gmail.com>
2015-12-17 11:58:48 +0100

    update paper

--- summary_paper/analysis/compare_to_financial_fragility.R
+++ summary_paper/analysis/compare_to_financial_fragility.R
@@ -11,6 +11,7 @@ library(repmis)
 library(dplyr)
 library(lubridate)
 library(countrycode)
+library(corrplot)
 library(DataCombine)
 library(WDI)
 library(ggplot2)
@@ -28,6 +29,25 @@ set_valid_wd(pos_directs)
 range01 <- function(x, na.rm = T){
     (x - min(x, na.rm = na.rm)) /
         (max(x, na.rm = na.rm) - min(x, na.rm = na.rm))
+}
+
+# Combined sig. tests function
+# From https://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
+cor.mtest <- function(mat, conf.level = 0.95) {
+    mat <- as.matrix(mat)
+    n <- ncol(mat)
+    p.mat <- lowCI.mat <- uppCI.mat <- matrix(NA, n, n)
+    diag(p.mat) <- 0
+    diag(lowCI.mat) <- diag(uppCI.mat) <- 1
+    for (i in 1:(n - 1)) {
+        for (j in (i + 1):n) {
+            tmp <- cor.test(mat[, i], mat[, j], conf.level = conf.level)
+            p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+            lowCI.mat[i, j] <- lowCI.mat[j, i] <- tmp$conf.int[1]
+            uppCI.mat[i, j] <- uppCI.mat[j, i] <- tmp$conf.int[2]
+        }
+    }
+    return(list(p.mat, lowCI.mat, uppCI.mat))
 }
 
 # Import FinStress
