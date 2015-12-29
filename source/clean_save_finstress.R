151f75305f4dab306f2b650f791c537c18d3fc61
christophergandrud <christopher.gandrud@gmail.com>
2015-12-29 09:43:02 +0100

    paper and readme improvements

--- source/clean_save_finstress.R
+++ source/clean_save_finstress.R
@@ -1,6 +1,5 @@
-# ---------------------------------------------------------------------------- #
-# Save cleaned FinStress for public use
-# Christopher Gandrud
+# ----------------------------------------------------------------------------
+# # Save cleaned FinStress for public use Christopher Gandrud
 # MIT License
 # ---------------------------------------------------------------------------- #
 
@@ -11,18 +10,17 @@ library(dplyr)
 library(DataCombine)
 
 # Set working directory
-possibles <- c('/git_repositories/EIUCrisesMeasure/')
+possibles <- c("/git_repositories/EIUCrisesMeasure/")
 
 set_valid_wd(possibles)
 
-# Load raw data 
-raw <- import('data/results_kpca_rescaled.csv')
+# Load raw data
+raw <- import("data/results_kpca_rescaled.csv")
 
-sub <- raw %>% select(iso2c, country, date, C1_ma) %>%
-        rename(FinStress = C1_ma)
+sub <- raw %>% select(iso2c, country, date, C1_ma) %>% rename(FinStress = C1_ma)
 
 # Drop NAs created by moving average
-sub <- sub %>% DropNA(c('iso2c', 'country', 'date', 'FinStress'))
+sub <- sub %>% DropNA(c("iso2c", "country", "date", "FinStress"))
 
 # Save
-export(sub, file = 'data/FinStress.csv')
+export(sub, file = "data/FinStress.csv")
