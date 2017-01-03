70a4936cd9eac6977c370304bff8ce34ee30961a
christophergandrud <christopher.gandrud@gmail.com>
2017-01-03 07:50:08 +0100

    preprocessed data saved

--- source/preprocess_eiu.R
+++ source/preprocess_eiu.R
@@ -5,6 +5,8 @@
 # ---------------------------------------------------------------------------- #
 
 # Load packages
+## Currently requires custom quanteda version due to a bug in the
+## official release.
 if (!('quanteda' %in% installed.packages()[, 1])) 
     devtools::install_github('christophergandrud/quanteda')
 library(quanteda)
@@ -15,6 +17,7 @@ library(repmis)
 library(lubridate)
 library(dplyr)
 library(stringr)
+library(countrycode)
 
 # Set working directory of parsed texts. Change as needed.
 pos_directs <- c('~/Desktop/eiu/eiu_extracted/',
@@ -30,6 +33,10 @@ date_country <- list.files() %>% gsub('\\.txt', '', .) %>%
 date_country[, 2] <- gsub('-', ' ', date_country[, 2])
 names(date_country) <- c('date', 'country')
 date_country$date <- ymd(date_country$date)
+date_country$iso3c <- countrycode(date_country$country,
+                                  origin = 'country.name',
+                                  destination = 'iso3c',
+                                  warn = TRUE)
 
 # Load corpus and preprocess
 texts_df <- readtext(file = list.files())
@@ -37,9 +44,13 @@ texts_df <- readtext(file = list.files())
 # Apply clean row names 
 texts_df <- cbind(texts_df, date_country)
 
+# Remove non-countries 
+texts_df <- subset(texts_df, !is.na(iso3c))
+
 # Remove texts from before 2003 due to inconsistent format
 texts_df_2003 <- subset(texts_df, date >= '2003-01-01')
-texts_2003_texts <- data.frame(texts_df_2003[, 1], stringsAsFactors = FALSE)
+texts_2003_texts <- data.frame(texts_df_2003[, 1], 
+                               stringsAsFactors = FALSE)
 
 # Create corpus
 eiu_corpus <- corpus(texts_2003_texts, text_field = 1)
@@ -48,22 +59,42 @@ eiu_corpus <- corpus(texts_2003_texts, text_field = 1)
 docvars(eiu_corpus, 'date') <- texts_df_2003$date
 docvars(eiu_corpus, 'country') <- texts_df_2003$country
 
-
 # Preprocess and convert to document-feature matrix
-eiu_token <- tokenize(eiu_corpus, stem = TRUE, removeNumbers = TRUE, 
-               removePunct = TRUE, removeSeparators = TRUE, removeSymbols = TRUE)
+eiu_token <- tokenize(eiu_corpus, removeNumbers = TRUE, 
+               removePunct = TRUE, removeSeparators = TRUE, 
+               removeSymbols = TRUE, removeHyphens = TRUE, 
+               removeTwitter = TRUE, removeURL = TRUE, 
+               verbose = TRUE)
 
+# For unknown reasons these functions do not work within tokenize
 eiu_token <- toLower(eiu_token)
-eiu_token <- removeFeatures(eiu_token, stopwords('english'))
+eiu_token <- removeFeatures(eiu_token, stopwords('SMART'))
+eiu_token <- tokens_wordstem(eiu_token)
+
+# Find documents with fewer than 5 tokens
+length_spec <- 5 # Assigns token length
+keep_vec <- vector()
+for (i in 1:length(eiu_token)) {
+    more_length <- length(eiu_token[[i]]) > length_spec
+    if (isTRUE(more_length)) keep_vec <- c(keep_vec, i)
+}
+
+# Collapse into a list of character vectors for each document
+eiu_list <- lapply(eiu_token, paste, collapse = ' ')    
+
+# Remove documents with fewer than 5 tokens
+eiu_list <- eiu_list[keep_vec]
+eiu_ids <- texts_df_2003[keep_vec, c('date', 'iso3c')]
+names(eiu_list) <- paste(eiu_ids[, 'iso3c'], eiu_ids[, 'date'],
+                         sep = '_')
+
+# Save preprocessed corpus in the git repository. Change as needed.
+pos_directs_git <- c('/git_repositories/EIUCrisesMeasure/source/')
+set_valid_wd(pos_directs_git)
+
+save(eiu_list, file = 'preprocessed_data/eiu_texts_from_2003.rda')
 
 
 
 
-clean_corpus_full <- Corpus(DirSource()) %>%
-    tm_map(removeWords,
-           stopwords(kind = "SMART"), mc.cores = 2) %>%
-    tm_map(stemDocument, mc.cores = 2) %>%
-    tm_map(stripWhitespace) %>%
-    # tm_map(content_transformer(tolower), mc.cores = 1) %>%
-    tm_map(removePunctuation, mc.cores = 2) %>%
-    tm_map(removeNumbers, mc.cores = 2) 
+eiu_dfm <- dfm(eiu_token)
